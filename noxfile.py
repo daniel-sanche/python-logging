536831b3a0fa729ccd695cae4b61c84a27628e69
Daniel Sanche <d.sanche14@gmail.com>
2020-10-14 16:30:59 -0700

    removed unit tests for python 2.7

--- noxfile.py
+++ noxfile.py
@@ -101,7 +101,7 @@ def default(session, django_dep=("django",)):
     )
 
 
-@nox.session(python=["2.7", "3.5", "3.6", "3.7"])
+@nox.session(python=UNIT_TEST_PYTHON_VERSIONS)
 def unit(session):
     """Run the unit test suite."""
 
